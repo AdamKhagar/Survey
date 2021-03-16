9d8ab0239ee8d0c2b03220301d5bd1ee1f1b1522
Adam Khagar <adamhagarov@gmail.com>
2021-03-16 20:37:04 +0300

    admin part

--- app/survey/admin.py
+++ app/survey/admin.py
@@ -1,5 +1,51 @@
 from django.contrib import admin
+from django.forms import ModelForm
 
 from . import models
 
-admin.site.register(models.Survey)
+
+class QuestionTextAnswerInline(admin.TabularInline):
+    model = models.QuestionTextAnswer
+    extra = 1
+
+
+class QuestionChoiceAnswerInline(admin.TabularInline):
+    model = models.QuestionChoiceAnswer
+    extra = 1
+
+
+class SurveyDataCreateOrReadOnly(ModelForm):
+    class Meta:
+        model = models.Survey
+        exclude = ('date',)
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionTextAnswerInline,
+        QuestionChoiceAnswerInline,
+    ]
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        if obj:
+            kwargs['form'] = SurveyDataCreateOrReadOnly
+        return super().get_form(request, obj, **kwargs)
+
+
+admin.site.register(models.Survey, SurveyAdmin)
+
+
+class AnswerChoiceInline(admin.TabularInline):
+    model = models.AnswerChoice
+    extra = 4
+
+
+class QuestionChoiceAnswer(admin.ModelAdmin):
+    inlines = [
+        AnswerChoiceInline
+    ]
+
+
+admin.site.register(models.QuestionChoiceAnswer, QuestionChoiceAnswer)
+
+admin.site.register(models.QuestionTextAnswer)
--- app/survey/models.py
+++ app/survey/models.py
@@ -1,16 +1,58 @@
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+from typing import List
+
 from django.db import models
 
+
+class Survey(models.Model):
+    """Опросник"""
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
+    def add_simple_question(self, question_text: str):
+        return QuestionTextAnswer.objects.create(
+            survey=self,
+            question_text=question_text
+        )
+    add_simple_question.short_description = 'Add Simple Text Question'
+
+    def add_question_choice(
+            self,
+            question_text: str,
+            choices: List[str],
+            choices_count: int = 1
+    ) -> 'QuestionChoiceAnswer':
+        if len(choices) > choices_count:
+            raise ValueError
+
+        question = QuestionChoiceAnswer.objects.create(
+            question_text=question_text,
+            survey=self,
+            choices_count=choices_count,
+        )
+        for choice in choices:
+            AnswerChoice.objects.create(
+                question=question,
+                answer_text=choice
+            )
+
+        return question
+    add_question_choice.boolean = True
 
 class QuestionBase(models.Model):
     """Базовый класс вопроса"""
     question_text = models.TextField()
 
+    class Meta:
+        abstract = True
+
 
 class QuestionTextAnswer(QuestionBase):
     """Вопрос с текстовым отвертом"""
-    answer = models.TextField()
+    survey = models.ForeignKey(
+        Survey,
+        related_name='question_text_answer',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f'#{self.pk}:QuestionText {self.question_text}'
@@ -19,6 +61,12 @@ class QuestionTextAnswer(QuestionBase):
 class QuestionChoiceAnswer(QuestionBase):
     """Вопрос с вариантами ответа"""
     choices_count = models.IntegerField()
+
+    survey = models.ForeignKey(
+        Survey,
+        related_name='question_choice_answer',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f'#{self.pk}:QuestionChoice {self.choices_count} {self.question_text}'
@@ -31,41 +79,7 @@ class AnswerChoice(models.Model):
         related_name='choices',
         on_delete=models.CASCADE,
     )
-    answer = models.TextField()
-
-
-class Survey(models.Model):
-    """Опросник"""
-    title = models.CharField(max_length=100)
-    date = models.DateTimeField()
-
-    def add_question(self, question: QuestionBase) -> 'SurveyQuestion':
-        question_type = ContentType.objects.get_for_model(question)
-
-        return SurveyQuestion.objects.create(
-            survey=self,
-            question_type=question_type,
-            question_object_id=question.pk
-        )
-
-
-class SurveyQuestion(models.Model):
-    """Many to many"""
-    survey = models.ForeignKey(
-        Survey,
-        on_delete=models.CASCADE,
-        related_name='questions',
-    )
-
-    question_object_id = models.IntegerField()
-    question_type = models.ForeignKey(
-        ContentType,
-        on_delete=models.PROTECT,
-    )
-    question = GenericForeignKey(
-        'question_type',
-        'question_object_id',
-    )
+    answer_text = models.TextField()
 
 
 class User(models.Model):
